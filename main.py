c4d9e3dc86545960d699f1aab4b32627b2039b29
Piorosen <aoikazto@gmail.com>
2020-10-17 21:24:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 from datetime import datetime
 from pytz import timezone
+
+import time
+import random
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-
-# test
-import time
-import random
 from github import Github
-
 def get_github_repo(access_token, repository_name):
     g = Github(access_token)
     repo = g.get_user().get_repo(repository_name)
@@ -18,11 +18,6 @@ def get_github_repo(access_token, repository_name):
 
 def upload_github_issue(repo, title, body):
     repo.create_issue(title=title, body=body)
-
-
-seoul_timezone = timezone('Asia/Seoul')
-today = datetime.now(seoul_timezone)
-today_data = today.strftime("%Y년 %m월 %d일 %H시 %M분 : %S초")
 
 access_token = os.environ['GITHUB_TOKEN']
 repository_name = "github-Action-HangKik"
@@ -37,35 +32,45 @@ options.add_argument("--no-sandbox")
 
 # chrome driver
 driver = webdriver.Chrome('chromedriver', chrome_options=options)
-
-# 원하는 url로 접속
-driver.get('https://forms.gle/ZouR2hTRsZqFVr4X8')
-driver.maximize_window()
-
-name = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-name.send_keys("차주형")
-
-number = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-number.send_keys("20193148")
-
-room = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-room.send_keys("B412")
-
-x = random.randrange(0, 9)
-
-
-fever = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
-fever.send_keys("35.7")
-
-etc = driver.find_element_by_xpath('//*[@id="i21"]/div[3]/div').click()
-
-login_btn = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
+def call(name, number, room, temperture):
+    # 원하는 url로 접속
+    driver.get('https://forms.gle/ZouR2hTRsZqFVr4X8')
+    driver.maximize_window()
+    name = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    name.send_keys(name)
+    number = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    number.send_keys(number)
+    room = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    room.send_keys(room)
+    fever = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    fever.send_keys(temperture)
+    etc = driver.find_element_by_xpath('//*[@id="i21"]/div[3]/div').click()
+    login_btn = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
 
 
+def makeBody(list):
+    result = ""
+    for item in list:
+        result += f"이름 : {item[0]}, 학번 : {item[1]}, 방번호 : {item[2]}, 체온 : {item[3]}\n"
+    return result
+
+
+
+seoul_timezone = timezone('Asia/Seoul')
+today = datetime.now(seoul_timezone)
+today_data = today.strftime("%Y년 %m월 %d일 %H시 %M분 : %S초")
+
+
+students = [ [ "차주형", "20183221", "B412" ] ]
+
+for student in students:
+    temp = rand.randrange(36.0, 37.0)
+    student.append(temp)
+    call(student[0], student[1], student[2], temp])
 
 repo = get_github_repo(access_token, repository_name)
-
 title = f"날짜 발열 테스트 : ({today_data})"
-body = "코드가 여기까지 돌았다면. 정상적으로 동작을 하였습니다. \n없다면 직접하시길 바랍니다. !!"
+
+body = makeBody(students)
 
 upload_github_issue(repo, title, body)
