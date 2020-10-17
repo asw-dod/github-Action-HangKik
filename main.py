a3f0c1c312bd50d1950479c16bc935f888c5cd38
Piorosen <aoikazto@gmail.com>
2020-10-17 20:11:02 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import time
+import random
+
+driver = webdriver.Chrome()
+
+# 원하는 url로 접속
+driver.get('https://forms.gle/ZouR2hTRsZqFVr4X8')
+driver.maximize_window()
+
+name = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+name.send_keys("황진주")
+
+number = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+number.send_keys("20193148")
+
+room = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+room.send_keys("B412")
+
+x = random.random(0, 9)
+
+
+fever = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
+fever.send_keys("35.7")
+
+etc = driver.find_element_by_xpath('//*[@id="i21"]/div[3]/div').click()
+
+login_btn = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
