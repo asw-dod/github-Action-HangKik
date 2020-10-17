f96aee14a24e53b01ade5fc4713a9b2f77d0768d
Piorosen <aoikazto@gmail.com>
2020-10-17 21:12:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+from datetime import datetime
+from pytz import timezone
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+
 # test
 import time
 import random
@@ -16,12 +19,13 @@ def get_github_repo(access_token, repository_name):
 def upload_github_issue(repo, title, body):
     repo.create_issue(title=title, body=body)
 
+
+seoul_timezone = timezone('Asia/Seoul')
+today = datetime.now(seoul_timezone)
+today_data = today.strftime("%Y년 %m월 %d일 %H시 %M분 : %S초")
+
 access_token = os.environ['GITHUB_TOKEN']
 repository_name = "github-Action-HangKik"
-
-repo = get_github_repo(access_token, repository_name)
-upload_github_issue(repo, "테스트 한다!", "그리고 바디다!")
-
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -56,3 +60,12 @@ fever.send_keys("35.7")
 etc = driver.find_element_by_xpath('//*[@id="i21"]/div[3]/div').click()
 
 login_btn = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
+
+
+
+repo = get_github_repo(access_token, repository_name)
+
+title = f"날짜 발열 테스트 : ({today_data})"
+body = "코드가 여기까지 돌았다면. 정상적으로 동작을 하였습니다. \n없다면 직접하시길 바랍니다. !!"
+
+upload_github_issue(repo, title, body)
