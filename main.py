62853e026be012ab5b0314b82602fcea5841929b
Piorosen <aoikazto@gmail.com>
2020-10-17 20:34:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import random
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument("lang=ko_KR")
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument("--no-sandbox")
+
+# chrome driver
+driver = webdriver.Chrome('chromedriver', chrome_options=options)
 
 # 원하는 url로 접속
 driver.get('https://forms.gle/ZouR2hTRsZqFVr4X8')
